841c498caa4381dfa31fdbab0a31875bc5f3fb40
GeetikaSharma-TCH <geetika@technocratshorizons.com>
2023-06-14 13:06:28 +0530

    Correct Spacing issue

--- src/Components/Login/ToDoForm/todoForm.jsx
+++ src/Components/Login/ToDoForm/todoForm.jsx
@@ -9,52 +9,41 @@ import ToDoList from '../ToDoList/todoList';
 import { Container, FormControl, TextField, Button } from '@mui/material';
 const TodoForm = () => {
 
-    // state
-    const [task, setTask] = useState('')
-    
-    // use selectr to get state data
-    const todo = useSelector((state) => state.todoList)
-    console.log('todo', todo)
+  // state
+  const [task, setTask] = useState('')
+  
+  // use selectr to get state data
+  const todo = useSelector((state) => state.todoList)
+  console.log('todo', todo)
 
- 
-
-    // to update and dispatch actions
-    const dispatch = useDispatch()
-    console.log('dispatch', dispatch)
-
-    // function to call dispatch
-    const dispatchAddTodo = (event) => {
-        console.log('event', event)
-        dispatch(addToddo(task))
-        event.preventDefault()
-        setTask('')
-    }
-
-    return (
-        <div>
-						<Container fixed>
-                <FormControl>
-									<TextField
-										id="outlined-password-input"
-										label="Enter Task"
-										type="text"
-										size="small"
-										margin="normal"
-										value={task} onChange={(event) => setTask(event.target.value)}
-									/>
-									{/* <TextField
-										id="outlined-password-input"
-										label="Enter details"
-										size="small"
-										type="text"
-										margin="normal"
-									/> */}
-									<Button variant="contained" margin="normal" onClick={dispatchAddTodo}>Add</Button>
-                </FormControl>
-                <ToDoList></ToDoList>
-						</Container>
-        </div>
-    );
+  // to update and dispatch actions
+  const dispatch = useDispatch()
+  console.log('dispatch', dispatch)
+  // function to call dispatch
+  const dispatchAddTodo = (event) => {
+    console.log('event', event)
+    dispatch(addToddo(task))
+    event.preventDefault()
+    setTask('')
+  }
+  return (
+    <div>
+			<Container fixed>
+        <FormControl>
+					<TextField
+						id="outlined-password-input"
+						label="Enter Task"
+						type="text"
+						size="small"
+						margin="normal"
+						value={task} onChange={(event) => setTask(event.target.value)}
+					/>
+					<Button variant="contained" margin="normal" onClick={dispatchAddTodo}>Add</Button>
+        </FormControl>
+          <ToDoList></ToDoList>
+			</Container>
+    </div>
+  );
 }
  
 export default TodoForm;
--- src/Components/Login/ToDoList/todoList.jsx
+++ src/Components/Login/ToDoList/todoList.jsx
@@ -7,68 +7,67 @@ import {Checkbox} from '@mui/material';
 
 const ToDoList = () => {
 
-    // to get data from store
-    const todoList =  useSelector((state) => state.todo.todoList )
-    console.log('todoList==-====', todoList)
-    
-    // dispatch
-    const dispatch = useDispatch()
-
-    const [open, setOpen] = useState(false)
-    return ( 
+  // to get data from store
+  const todoList =  useSelector((state) => state.todo.todoList )
+  console.log('todoList==-====', todoList)
+  
+  // dispatch
+  const dispatch = useDispatch()
+  const [open, setOpen] = useState(false)
+  return ( 
+    <div>
+      <TableContainer>
+          <Table  sx={{ minWidth: 650 }} size="small" stickyHeader aria-label="sticky table">  
+            <TableHead>
+                <TableRow>
+                    <TableCell><Checkbox/></TableCell>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Actions</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+              {todoList.map((val, key) => {
+								return(
+									<TableRow key={key}>
+										<TableCell>
+												<Checkbox/>
+										</TableCell>
+										<TableCell>{val.id}</TableCell>
+										<TableCell>{val.name}</TableCell>
+										<TableCell>
+											<ButtonGroup variant="text" aria-label="text button group">
+												<Button onClick={() => {
+													setOpen(true)
+													dispatch(editTodo(val.id))
+												}}>Edit	
+												</Button>
+												<Button  onClick={() => {
+													dispatch(deleteTodo(val.id))
+												}}>Delete
+												</Button>
+											</ButtonGroup>
+										</TableCell>
+									</TableRow>
+								)
+            	})}
+							{todoList.length === 0 && 
+								<TableRow align="center">
+									<TableCell>
+										No Record Found
+									</TableCell>
+								</TableRow>
+							}
+            </TableBody>
+          </Table>
+      </TableContainer>
         <div>
-            <TableContainer>
-                <Table  sx={{ minWidth: 650 }} size="small" stickyHeader aria-label="sticky table">  
-                    <TableHead>
-                        <TableRow>
-                            <TableCell><Checkbox/></TableCell>
-                            <TableCell>Id</TableCell>
-                            <TableCell>Name</TableCell>
-                            <TableCell>Actions</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {todoList.map((val, key) => {
-                        return(
-                            <TableRow key={key}>
-                                <TableCell>
-                                    <Checkbox/>
-                                </TableCell>
-                                <TableCell>{val.id}</TableCell>
-                                <TableCell>{val.name}</TableCell>
-																<TableCell>
-																<ButtonGroup variant="text" aria-label="text button group">
-																	<Button onClick={() => {
-																					setOpen(true)
-																					dispatch(editTodo(val.id))
-																			}}>Edit	
-																	</Button>
-  																<Button  onClick={() => {
-                                        dispatch(deleteTodo(val.id))
-                                    }}>Delete
-																	</Button>
-																</ButtonGroup>
-																</TableCell>
-                            </TableRow>
-                        )
-                    })}
-										{todoList.length === 0 && 
-												<TableRow align="center">
-													<TableCell>
-														No Record Found
-													</TableCell>
-												</TableRow>
-										}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <div>
-            </div>
-            {open &&
-                <UpdateModal open={open} setOpen={setOpen}></UpdateModal>
-            }
         </div>
-    );
+        {open &&
+            <UpdateModal open={open} setOpen={setOpen}></UpdateModal>
+        }
+    </div>
+  );
 }
  
 export default ToDoList;
